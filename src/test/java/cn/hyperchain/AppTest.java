eee399d3fe3ba20792d797a5b0ebe1bd5a3cc819
skyswt <2515767544@qq.com>
2020-03-25 13:47:21 +0800

    添加依赖以及配置项目属性

--- src/test/java/cn/hyperchain/AppTest.java
+++ src/test/java/cn/hyperchain/AppTest.java
@@ -3,10 +3,15 @@ package cn.hyperchain;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * Unit test for simple App.
  */
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class AppTest 
 {
     /**
@@ -15,6 +20,6 @@ public class AppTest
     @Test
     public void shouldAnswerWithTrue()
     {
-        assertTrue( true );
+
     }
 }
